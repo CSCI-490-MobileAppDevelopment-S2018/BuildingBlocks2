dda88ee38ed59e5e96d3ac9db0e277b7a6754164
kbriggs <kabriggs991@gmail.com>
2017-02-06 23:05:34 -0500

    video

--- app/src/main/java/edu/cofc/buildingblocks2/MainActivity.java
+++ app/src/main/java/edu/cofc/buildingblocks2/MainActivity.java
@@ -1,14 +1,17 @@
 package edu.cofc.buildingblocks2;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Window;
 import android.widget.Chronometer;
+import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       final Chronometer timer = (Chronometer) findViewById(R.id.chronometer);
-        long base = timer.getBase();
-        Log.d("DebugTag", "base = " + base);
-        timer.setBase(90978530);
-        timer.start();
+        VideoView vv = (VideoView) findViewById(R.id.video_view);
+        MediaController mc = new MediaController(this);
+        Uri video = Uri.parse("http://andys-veggie-garden.appspot.com/vid/reveal.mp4");
+
+        vv.setMediaController(mc);
+        vv.setVideoURI(video);
     }
 
 
