228acc558be0bbcd8b9ff7457fd7efc55a4d79d4
kbriggs <kabriggs991@gmail.com>
2017-02-06 20:59:30 -0500

    Attempt at actionbar

--- app/src/main/java/edu/cofc/buildingblocks2/MainActivity.java
+++ app/src/main/java/edu/cofc/buildingblocks2/MainActivity.java
@@ -2,6 +2,8 @@ package edu.cofc.buildingblocks2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
         ProgressBar bar = (ProgressBar) findViewById(R.id.progress_bar);
         bar.setProgress(75);
+
+        supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        supportRequestWindowFeature(Window.FEATURE_PROGRESS);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_progress);
     }
+
+
 }
